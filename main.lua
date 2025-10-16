d1acf50a83ebb0af11e874a6fd94ec4b5c07d965
João Lucas <jlmacena06@gmail.com>
2025-10-16 12:58:38 -0300

    Main menu initial implementation

--- main.lua
+++ main.lua
@@ -2,14 +2,16 @@
 
 _G.love = require("love")
 
-local BUTTON_WIDTH = 2/3 -- 15% of total Screen
+local BUTTON_WIDTH = 2/3 
 local BUTTON_HEIGHT = 0.1
 local buttons = {}  -- buttons table
 
 function newButton(text , fn)
     return {
         text=text,
-        fn = fn
+        fn = fn,
+        last = false, 
+        now = true
     }
 end
 
@@ -35,20 +37,26 @@ end
 function love.update()
     
 end
-
-
-function love.draw()
+ function love.draw()
 
     local cursor_y= 0
     for i,b in ipairs(buttons) do
+        
         local by = (sh * 0.5)  - (total_btn_heigth * 0.5 ) + cursor_y
 
-        love.graphics.setColor(0.5,0.5,0.5, 1)
+        -- The mous is hover this button 
+        hot = love.mouse.getX()  > button_init and love.mouse.getX() < button_init + BUTTON_WIDTH
+                and love.mouse.getY() > by and love.mouse.getY() < by + BUTTON_HEIGHT
+        if hot then  love.graphics.setColor(1,1,1, 1)
+        else love.graphics.setColor(0.5,0.5,0.5, 1) end
+
+
+   
         love.graphics.rectangle("fill",
-                                button_init,                  --x 
-                                by,                      --y
-                                BUTTON_WIDTH , -- Width 
-                                BUTTON_HEIGHT                     -- height  
+                                button_init,            
+                                by,                   
+                                BUTTON_WIDTH ,         
+                                BUTTON_HEIGHT                     
                             )
 
         textW = font:getWidth(b.text)
@@ -60,6 +68,16 @@ function love.draw()
                             by + (texth*0.5))
 
 
-        cursor_y = cursor_y + (BUTTON_HEIGHT + margin_buttons)
+        cursor_y = cursor_y + (BUTTON_HEIGHT + margin_buttons) 
+        
+        -- Click Logic. 
+        b.now = love.mouse.isDown(1)
+        if b.now and hot and not b.last then 
+    
+            b.fn()
+        end
+    
+        b.last = b.now
     end 
+    
 end
