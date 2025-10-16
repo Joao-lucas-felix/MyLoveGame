372d5b8da88f60120f5cc984154965221b0b38e3
João Lucas <jlmacena06@gmail.com>
2025-10-15 21:29:47 -0300

    Init of the game start menu development

--- main.lua
+++ main.lua
@@ -1,1 +1,65 @@
+-- This menu works if the screen dimensios are not resizeble
+
 _G.love = require("love")
+
+local BUTTON_WIDTH = 2/3 -- 15% of total Screen
+local BUTTON_HEIGHT = 0.1
+local buttons = {}  -- buttons table
+
+function newButton(text , fn)
+    return {
+        text=text,
+        fn = fn
+    }
+end
+
+function love.load()
+    font = love.graphics.newFont("fonts/PixelGameFont.ttf",32 )
+    table.insert(buttons, newButton("NEW GAME", function() print("New Game Started!") end))
+    table.insert(buttons, newButton("EXIT", function() love.event.quit(0) end))
+    total_buttons = #buttons
+    margin_buttons = 20
+
+    -- Screen dimensions
+    sw = love.graphics.getWidth()
+    sh = love.graphics.getHeight()
+
+    BUTTON_WIDTH = BUTTON_WIDTH*sw
+    BUTTON_HEIGHT = BUTTON_HEIGHT*sh
+    button_init = (sw*0.5) - (BUTTON_WIDTH*0.5)
+    -- Calcula a altura total dos buttons com base na proporção deles 15%, + a margem.
+    total_btn_heigth = total_buttons *( BUTTON_HEIGHT + (margin_buttons))
+end
+
+
+function love.update()
+    
+end
+
+
+function love.draw()
+
+    local cursor_y= 0
+    for i,b in ipairs(buttons) do
+        local by = (sh * 0.5)  - (total_btn_heigth * 0.5 ) + cursor_y
+
+        love.graphics.setColor(0.5,0.5,0.5, 1)
+        love.graphics.rectangle("fill",
+                                button_init,                  --x 
+                                by,                      --y
+                                BUTTON_WIDTH , -- Width 
+                                BUTTON_HEIGHT                     -- height  
+                            )
+
+        textW = font:getWidth(b.text)
+        texth = font:getHeight(b.text)
+
+        love.graphics.setColor(0,0,0,1)
+        love.graphics.print(b.text, font,
+                            sw*0.5 - textW*0.5  ,
+                            by + (texth*0.5))
+
+
+        cursor_y = cursor_y + (BUTTON_HEIGHT + margin_buttons)
+    end 
+end
